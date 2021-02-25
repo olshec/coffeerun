7559e046800d3398f8d33026918c719719a4b19f
Oleg Shestakov <olshec@gmail.com>
2021-02-25 12:14:44 +0300

    fixed addSubmitHandler

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
 (function (window) {
+    const FORM_SELECTOR = '[data-coffee-order="form"]';
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore;
+    let FormHandler = App.FormHandler;
     let myTruck = new Truck('ncc-1701', new DataStore());
     window.myTruck = myTruck;
+    let formHandler = new FormHandler(FORM_SELECTOR);
+    
+    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
+    console.log(formHandler);
 })(window);
