445e2dd1d9e5432dd9c4697b11a4e7728b41e875
Oleg Shestakov <olshec@gmail.com>
2021-03-02 18:48:29 +0300

    added RemoteDataStore

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,16 @@
 (function (window) {
     const FORM_SELECTOR = '[data-coffee-order="form"]';
     const CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+    const SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';   
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore;
+    let RemoteDataStore = App.RemoteDataStore;
     let FormHandler = App.FormHandler;
     let Validation = App.Validation;
     let CheckList = App.CheckList;
-    let myTruck = new Truck('ncc-1701', new DataStore());
+    let remoteDS = new RemoteDataStore(SERVER_URL);
+    let myTruck = new Truck('ncc-1701', remoteDS);
     window.myTruck = myTruck;
     let checkList = new CheckList(CHECKLIST_SELECTOR);
     checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck))
